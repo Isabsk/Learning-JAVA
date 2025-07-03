c31ad6b2d47c353e1259549757fa9ecf290b97e3
Isabsk <isabsk10@gmail.com>
2025-07-03 12:37:51 +0530

    Export Project00 folder and its contents

---
+++ Project00/PassChecker.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PassChecker {
+    public static void main(String[] args) {
+        String password = "P@ssw0rd123";
+        Scanner sc = new Scanner(System.in);
+        String userInput;
+        do {
+            System.out.print("Enter your password: ");
+            userInput = sc.nextLine();
+            if (userInput.equals(password)) {
+                System.out.println("Access granted.");
+                break;
+            }
+            else {
+                System.out.println("Access denied. Try again.");
+            }
+        } while (true);
+    }
+}
